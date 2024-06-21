589a579aa7d4b96392717eb82a8f0abcd8eab0c9
SusNimo <166434158+SUSNIMO@users.noreply.github.com>
2024-06-21 13:23:46 +1200

    bars Test1

--- Matt's Version/Code/Web server (esp32)/SuperMon.h
+++ Matt's Version/Code/Web server (esp32)/SuperMon.h
@@ -226,6 +226,7 @@ table {
         width: 30%;
         height: 100%;
         position: relative;
+		display: flex;
     }
 	
 	.Pchart {
@@ -233,18 +234,19 @@ table {
         width: 30%;
         height: 100%;
         position: relative;
+		display: flex;
     }
 	
     .bar {
         background-color: #4db74a;
-        width: 5px; /* Width of each bar */
+        width: 1.1%;
         position: absolute;
         bottom: 0;
     }
 	
 	.barw {
         background-color: #007bff;
-        width: 5px; /* Width of each bar */
+        width: 1.1%;
         position: absolute;
         bottom: 0;
     }
@@ -258,6 +260,7 @@ table {
 
     .barAB { left: 300px; }
 
+	<!--
     //HEH bars
     .bar1 { left: 1%; }
     .bar2 { left: 1%; }
@@ -360,7 +363,7 @@ table {
     .bar99 { left: 98%; }
     .bar100 { left: 99%; }
     
-	
+	-->
 	.line {
 			position: absolute;
 			bottom: 0;
@@ -434,6 +437,10 @@ table {
       <tr>
         <td><div class="bodytext">Temperature</div></td>
         <td><div class="tabledata" id = "b1"></div></td>
+      </tr>
+	  <tr>
+        <td><div class="bodytext">Alarm</div></td>
+        <td><div class="tabledata" id = "A1"></div></td>
       </tr>
       </table>
     </div>
@@ -780,7 +787,7 @@ table {
 
 	//debugging 
 	
-	/*
+	
 	
 	var Pdata = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
     var Wdata = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
@@ -791,7 +798,7 @@ table {
 	WupdateBarHeights();
 	PupdateBarHeights();
 	
-	*/
+	
 	
   function DdisplayInput() {
     var userInput = document.getElementById("DinputField").value;
@@ -1043,6 +1050,10 @@ function WupdatePressureReadings(newReading) {
       document.getElementById("b1").style.width=(width+"%");
       document.getElementById("b1").style.backgroundColor=color;
       //document.getElementById("b1").style.borderRadius="5px";
+	  
+	  xmldoc = xmlResponse.getElementsByTagName("Alarm");
+      message = xmldoc[0].firstChild.nodeValue;
+		document.getElementById("A1").innerHTML=message;
     }
   
   
